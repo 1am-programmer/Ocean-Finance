802a3416be034b5e26394b4fd13fdd80bb9a5499
Danny <dannydotdev@gmail.com>
2024-12-21 19:15:12 +0100

    Typo

--- components/About.tsx
+++ components/About.tsx
@@ -23,11 +23,13 @@ const About = () => {
         Working on-chain with the best community
       </p>
 
-      <div className="flex py-20 gap-10 justify-center items-center">
+      <div
+        className={`${jost.className} flex py-20 gap-10 justify-center items-center    `}
+      >
         <section>
-          <Card className="bg-blue-300 w-[500px] py-8 text-center  px-5 ">
+          <Card className="bg-blue-200 w-[500px] py-8 text-center  px-5 ">
             <CardHeader>
-              <CardTitle>Borrowing</CardTitle>
+              <CardTitle className="text-xl">Borrowing on Eth</CardTitle>
               <CardDescription>Card Description</CardDescription>
             </CardHeader>
             <CardContent>
@@ -37,19 +39,19 @@ const About = () => {
                 alt="bond"
                 height="700"
                 width="700"
-                className="h-[200px] w-[200px]"
+                className="h-[200px] w-[200px]  flex  justify-center"
               />
             </CardContent>
             <CardFooter>
-              <Button className="bg-blue-600 w-full">Discover</Button>
+              <Button className="bg-blue-600 w-full py-5">Discover</Button>
             </CardFooter>
           </Card>
         </section>
 
         <section>
-          <Card className="bg-orange-300 w-[500px] py-8 text-center  px-5 ">
+          <Card className="bg-orange-200 w-[500px] py-8 text-center  px-5 ">
             <CardHeader>
-              <CardTitle>Lending</CardTitle>
+              <CardTitle className="text-xl">Lending on Eth </CardTitle>
               <CardDescription>Card Description</CardDescription>
             </CardHeader>
             <CardContent>
@@ -59,11 +61,11 @@ const About = () => {
                 alt="bond"
                 height="700"
                 width="700"
-                className="h-[200px] w-[200px]"
+                className="h-[200px] w-[200px] flex justify-center "
               />
             </CardContent>
             <CardFooter>
-              <Button className="bg-orange-600  w-full">Discover</Button>
+              <Button className="bg-orange-600  w-full  py-5">Discover</Button>
             </CardFooter>
           </Card>
         </section>
