dded76d8f750603e4262a7dba5643bf2e102ef57
Danny <dannydotdev@gmail.com>
2024-12-23 17:51:02 +0100

    community links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import { FaDiscord, FaTelegram, FaXTwitter } from "react-icons/fa6";
+import Link from "next/link";
+
 import { Jost } from "next/font/google";
 
 const jost = Jost({
@@ -25,6 +28,27 @@ const Footer = () => {
         <p className={`${jost.className} text-xs text-gray-400`}>
           All rights reserved
         </p>
+      </section>
+
+      <section className="flex gap-3">
+        <Link
+          href={"http://x.com/0x_Danny_"}
+          className="text-2xl hover:text-blue-500"
+        >
+          <FaXTwitter />
+        </Link>
+        <Link
+          href={" https://t.me/OxcEther"}
+          className="text-2xl hover:text-blue-500"
+        >
+          <FaTelegram />
+        </Link>
+        <Link
+          href={"http://discordapp.com/users/1151028385108213760"}
+          className="text-2xl hover:text-blue-500"
+        >
+          <FaDiscord />
+        </Link>
       </section>
     </div>
   );
--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { FaDiscord, FaTelegram, FaTwitter } from "react-icons/fa6";
+import { FaDiscord, FaTelegram, FaXTwitter } from "react-icons/fa6";
 import { Poppins } from "next/font/google";
+import Link from "next/link";
 
 const poppins = Poppins({
   weight: "400",
@@ -8,17 +9,34 @@ const poppins = Poppins({
 });
 const Socials = () => {
   return (
-    <div className="bg-gray-400 rounded-xl py-10 px-10 ">
-      <p className={`${poppins.className} text-xl`}>Join our community !</p>
-      <p>
-        Be the first to hear about community announcements and join the
-        discussion!
-      </p>
+    <div className="bg-gray-400 rounded-xl py-10 px-10">
+      <section className=" flex flex-col gap-3  ">
+        <p className={`${poppins.className} text-xl`}>Join our community !</p>
+        <p>
+          Be the first to hear about community announcements and join the
+          discussion!
+        </p>
 
-      <section className="flex gap-3">
-        <FaTwitter />
-        <FaDiscord />
-        <FaTelegram />
+        <section className="flex gap-3">
+          <Link
+            href={"http://x.com/0x_Danny_"}
+            className="text-2xl hover:text-blue-500"
+          >
+            <FaXTwitter />
+          </Link>
+          <Link
+            href={" https://t.me/OxcEther"}
+            className="text-2xl hover:text-blue-500"
+          >
+            <FaTelegram />
+          </Link>
+          <Link
+            href={"http://discordapp.com/users/1151028385108213760"}
+            className="text-2xl hover:text-blue-500"
+          >
+            <FaDiscord />
+          </Link>
+        </section>
       </section>
     </div>
   );
